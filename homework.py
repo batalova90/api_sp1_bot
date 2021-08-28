888d97a22c0a2281f1c3761c541be3a7ba533ed9
Batalova Anastasiya <batalova.av@gmail.com>
2021-08-28 11:35:45 +0300

    add bot

--- homework.py
+++ homework.py
@@ -1,9 +1,12 @@
+import logging
 import os
 import time
-import requests
-import telegram
-from dotenv import load_dotenv
+from logging.handlers import RotatingFileHandler
 
+import requests
+from dotenv import load_dotenv
+from requests.exceptions import HTTPError
+from telegram import Bot
 
 load_dotenv()
 
@@ -11,41 +14,73 @@ PRAKTIKUM_TOKEN = os.getenv('PRAKTIKUM_TOKEN')
 TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
 CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
 
-# проинициализируйте бота здесь,
-# чтобы он был доступен в каждом нижеобъявленном методе,
-# и не нужно было прокидывать его в каждый вызов
-bot = ...
+bot = Bot(token=TELEGRAM_TOKEN)
+
+# логирование
+logging.basicConfig(
+    level=logging.DEBUG,
+    filename='main.log', filemode='w',
+    format='%(asctime)s, %(levelname)s, %(name)s, %(message)s'
+)
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+handler = RotatingFileHandler('bot_logger.log',
+                              maxBytes=4086,
+                              backupCount=2)
+logger.addHandler(handler)
 
 
 def parse_homework_status(homework):
-    homework_name = ...
-    if ...
-        verdict = 'К сожалению, в работе нашлись ошибки.'
+    """В зависимости от статуса ф-ция формирует сообщение"""
+    homework_name = homework.get('homework_name')
+    if homework_name is None:
+        return logger.error('Отсутствует имя проекта')
+    status = homework.get('status')
+    msg = {'rejected': 'К сожалению, в работе нашлись ошибки.',
+           'approved': 'Ревьюеру всё понравилось, работа зачтена!',
+           'reviewing': 'Работа взята в ревью'}
+    if status not in msg:
+        logger.error('Неизвестный статус работы!')
     else:
-        verdict = 'Ревьюеру всё понравилось, работа зачтена!'
-    return f'У вас проверили работу "{homework_name}"!\n\n{verdict}'
+        return f'У вас проверили работу "{homework_name}"!\n\n{msg[status]}'
 
 
 def get_homeworks(current_timestamp):
-    homework_statuses = ...
-    return homework_statuses.json()
+    url = 'https://praktikum.yandex.ru/api/user_api/homework_statuses/'
+    headers = {'Authorization': f'OAuth {PRAKTIKUM_TOKEN}'}
+    payload = {'from_date': current_timestamp}
+    try:
+        homework_statuses = requests.get(url,
+                                         headers=headers,
+                                         params=payload)
+        # homework_statuses.raise_for_status()
+        return homework_statuses.json()
+    except HTTPError as http_err:
+        return logger.error(f'HTTP error: {http_err}')
+    except Exception as err:
+        return logger.error(f'Other error: {err}')
 
 
 def send_message(message):
-    return bot.send_message(...)
+    return bot.send_message(chat_id=CHAT_ID, text=message)
 
 
 def main():
     current_timestamp = int(time.time())  # Начальное значение timestamp
-
+    # current_timestamp = 1622505600
     while True:
         try:
-            ...
-            time.sleep(5 * 60)  # Опрашивать раз в пять минут
-
+            logger.debug('Бот запущен')
+            homeworks = get_homeworks(current_timestamp).get('homeworks')
+            if len(homeworks) != 0:
+                send_message(parse_homework_status(homeworks[0]))
+                logger.info('Бот отправил сообщение')
+            time.sleep(5 * 60)
         except Exception as e:
-            print(f'Бот упал с ошибкой: {e}')
-            time.sleep(5)
+            msg_error = f'Бот упал с ошибкой: {e}'
+            logger.error(msg_error)
+            bot.send_message(chat_id=CHAT_ID, text=msg_error)
+            time.sleep(10)
 
 
 if __name__ == '__main__':
